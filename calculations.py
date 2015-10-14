d6688f6aaa6a04573c923ec8ec2eebc8867c5d66
Andreas Lelli <andreas@lelli.se>
2015-10-14 12:21:13 +0200

    Added function to go through a folder of drag/lift files and find the optimal one
    Former-commit-id: 9fa92b849c8fb53e50d61e628855975d02f00121

--- calculations.py
+++ calculations.py
@@ -1,3 +1,5 @@
+import operator
+import os
 def liftdragratio(filename):
   numberofLines = 0
   lift = 0.0
@@ -14,8 +16,21 @@ def liftdragratio(filename):
     lift = lift/numberofLines
     drag = drag/numberofLines
     liftdrag = lift/drag
-    print lift
-    print drag
-    print liftdrag
+    #print filename
+    #print liftdrag
+    dict = {filename: liftdrag}
+    return dict
 #Only to make testing easier
 #liftdragratio('drag_ligt.m')
+
+#Take angle.m from 0-num
+def findOptimal(folder):
+  angles = {}
+  anglesTest = {'0.m':3237, '3.m':6984, '6m':3900}
+  for f in os.listdir(folder):
+    #add check to only do this for .m files
+    if f.endswith(".m"):
+      angles.update(liftdragratio(folder + '/' + f))
+  print angles
+  print max(angles, key=angles.get)
+findOptimal('results')
