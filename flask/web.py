31cf9466a1fd4809558f805607365b4149dedb44
cjoh88 <cjoh193@gmail.com>
2015-10-17 22:37:06 +0200

    Messing around and fixing some stuff
    Former-commit-id: c0436bea79ecd38a03e0486dfa51c358db36e009

--- flask/web.py
+++ flask/web.py
@@ -26,9 +26,7 @@ def index():
 
 @app.route('/generate', methods=['POST'])
 def generate():
-    print("Hello")
     if request.method == 'POST':
-        print("World")
         arg1 = request.form['angle_start']
         arg2 = request.form['angle_stop']
         arg3 = request.form['n_angles']
@@ -43,6 +41,22 @@ def generate():
         return redirect(url_for('index'))
     return redirect(url_for('index'))
 
+#@app.route('/generating/<listOfObjects>')
+#def generating_meshes(listOfObjects):
+    #if request.method == 'POST':
+    #    arg1 = request.form['angle_start']
+    #    arg2 = request.form['angle_stop']
+    #    arg3 = request.form['n_angles']
+    #    arg4 = request.form['n_nodes']
+    #    arg5 = request.form['n_levels']
+    #    arg6 = request.form['speed']
+    #    arg7 = request.form['n_vm']
+        #print(arg3)
+        ##command = RUN_PATH + "run.sh " + arg1 + ' ' + arg2 + ' ' + arg3 + ' ' + arg4 + ' ' + arg5
+    #    print(command)
+    #    return_code = call(command, shell=True)
+    #    return redirect(url_for('index'))
+    #return render_template('generating.html')
 
 
 
