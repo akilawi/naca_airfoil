30b1b9d7cffe81614ca8fe5b6ca3c5e1a3b64c5a
Ahmad Ilaiwi <ahmad.ilaiwi@gmail.com>
2015-10-19 13:23:52 +0200

    added group of tasks

--- run.py
+++ run.py
@@ -2,15 +2,19 @@
 from subprocess import call
 from tasks import runMsh
 import sys
+from celery import group
 
-TASK_QUEUE=[]
+
+
 def splitTasks(angle_start, angle_stop, n_angles, n_nodes , n_levels, speed):
+		jobs=[]
         anglediff= (angle_stop-angle_start)/n_angles
         for i in range(n_angles):
                 angle=angle_start+anglediff*i
                 exist=False
                 # CHECK IF ALEADY EXIST
-
                 if(not(exit)):
-                	TASK_QUEUE.append(runMsh.delay(angle , n_nodes , n_levels, speed))
-                
+                	jobs.append(runMsh.s(angle , n_nodes , n_levels, speed))
+        tasksGroup=group(jobs)
+        result = tasksGroup.apply_async()
+        #add the evaluated results to the file
--- tasks.py
+++ tasks.py
@@ -1,11 +1,13 @@
 
 from subprocess import call
 from celery import Celery
+import calculations
 app = Celery('tasks', backend='rpc://', broker='amqp://')
 @app.task
 def runMsh(angle , n_nodes , n_levels,speed):
         command='bash runfirst.sh %d %d %d %d %d %d' % (angle , angle , 1 , n_nodes , n_levels , speed)
         print call(command, shell=True)
+        return calculations.liftdragratio('navier_stokes_solver/results/%d.m'%angle)
 
 
 
