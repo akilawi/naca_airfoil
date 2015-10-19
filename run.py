25df6532cd372364d3d730d95a994593e7a1f759
Ahmad Ilaiwi <ahmad.ilaiwi@gmail.com>
2015-10-19 10:10:58 +0200

    fix bugs

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ def splitTasks(angle_start, angle_stop, n_angles, n_nodes , n_levels, speed):
 
 
 if __name__ == "__main__":
-   splitTasks(int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3]),int(sys.argv[4]),int(sys.argv[5]),i$
+   splitTasks(int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3]),int(sys.argv[4]),int(sys.argv[5]),int(sys.argv[6])
 
 
 
--- tasks.py
+++ tasks.py
@@ -3,7 +3,7 @@
 from celery import Celery
 app = Celery('tasks', backend='rpc://', broker='amqp://')
 def runMsh(angle , n_nodes , n_levels,speed):
-        command='bash runfirst.sh %d %d %d %d %d %d' % (angle , angle , 1 , n_nodes , n_levels , spe$
+        command='bash runfirst.sh %d %d %d %d %d %d' % (angle , angle , 1 , n_nodes , n_levels , speed)
         print call(command, shell=True)
 
 
