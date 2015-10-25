5d9359f19d8d2f110f9ece0d5e6acb63c98aca9c
Ahmad Ilaiwi <ahmad.ilaiwi@gmail.com>
2015-10-25 16:39:35 +0100

    fix bug

--- tasks.py
+++ tasks.py
@@ -2,7 +2,7 @@ import sys
 from subprocess import call
 from celery import Celery
 import calculations
-app = Celery('tasks', backend='rpc://', broker='amqp://guest:guest@%d//'%int(sys.argv[1])
+app = Celery('tasks', backend='rpc://', broker='amqp://')
 @app.task
 def runMsh(angle , n_nodes , n_levels,speed):
         command='bash runfirst.sh %d %d %d %d %d %d' % (angle , angle , 1 , n_nodes , n_levels , speed)
