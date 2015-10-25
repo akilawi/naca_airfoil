863ff725ac0c8dfe7c47fb8d2f78bdbc50a393a0
Ahmad Ilaiwi <ahmad.ilaiwi@gmail.com>
2015-10-25 16:15:06 +0100

    pass the master ip as arg to task

--- tasks.py
+++ tasks.py
@@ -1,8 +1,8 @@
-
+import sys
 from subprocess import call
 from celery import Celery
 import calculations
-app = Celery('tasks', backend='rpc://', broker='amqp://')
+app = Celery('tasks', backend='rpc://', broker='amqp://guest:guest@%d//'%sys.argv[1])
 @app.task
 def runMsh(angle , n_nodes , n_levels,speed):
         command='bash runfirst.sh %d %d %d %d %d %d' % (angle , angle , 1 , n_nodes , n_levels , speed)
