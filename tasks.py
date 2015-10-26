6043c98294e2f4f25aeaf10821af3f9f54a3701c
Andreas Lelli <andreas@lelli.se>
2015-10-26 11:26:19 +0100

    Added fixes for brokers/workers with dynamic IP etc

--- tasks.py
+++ tasks.py
@@ -2,7 +2,10 @@ import sys
 from subprocess import call
 from celery import Celery
 import calculations
-app = Celery('tasks', backend='rpc://', broker='amqp://')
+import os
+
+broker_ip = os.environ["BROKER_IP"]
+app = Celery('tasks', backend='amqp', broker='amqp://group5:group5@' + broker_ip + ':5672/naca')
 @app.task
 def runMsh(angle , n_nodes , n_levels,speed):
         command='bash runfirst.sh %d %d %d %d %d %d' % (angle , angle , 1 , n_nodes , n_levels , speed)
