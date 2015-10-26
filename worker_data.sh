c55316f11ba1d38539743fc5ae4e78efc6fa4144
Andreas Lelli <andreas@lelli.se>
2015-10-26 15:08:25 +0100

    Experimenting with user data, had some problems with yml

---
+++ worker_data.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+pip install celery
+git clone https://github.com/akilawi/naca_airfoil.git
+mv naca_airfoil /home/ubuntu/
+mkdir /home/ubuntu/naca_airfoil/geo
+mkdir /home/ubuntu/naca_airfoil/msh
+mkdir /home/ubuntu/naca_airfoil/xml
+chmod -R 777 /home/ubuntu/naca_airfoil
+cd /home/ubuntu/naca_airfoil
+sudo -H -u ubuntu bash -c "celery worker -A tasks &"
