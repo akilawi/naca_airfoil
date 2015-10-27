0d2803ec233e5e62882003f10f5fe2abc446327e
Andreas Lelli <andreas@lelli.se>
2015-10-27 09:41:22 +0100

    Fixes

--- tasks.py
+++ tasks.py
@@ -4,7 +4,7 @@ from celery import Celery
 import calculations
 import os
 
-broker_ip = os.environ["BROKER_IP"]
+broker_ip = os.environ["BROKERIP"]
 broker_adress = 'amqp://group5:group5@' + broker_ip + ':5672/naca'
 app = Celery('tasks', backend='amqp', broker=broker_adress)
 #app = Celery('tasks', backend='amqp', broker='amqp://group5:group5@130.238.29.6:5672/naca')
@@ -13,10 +13,3 @@ def runMsh(angle , n_nodes , n_levels,speed):
         command='bash runfirst.sh %d %d %d %d %d %d' % (angle , angle , 1 , n_nodes , n_levels , speed)
         print call(command, shell=True)
         return calculations.liftdragratio('navier_stokes_solver/results/%d.m' % angle)
-
-
-
-
-
-
-
--- worker_data.sh
+++ worker_data.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-pip install celery
-sudo pip install python-novaclient
 git clone https://github.com/akilawi/naca_airfoil.git
 mv naca_airfoil /home/ubuntu/
+cd /home/ubuntu/naca_airfoil
+ip_file=$(head -n 1 ip_number.txt)
+export BROKERIP=$ip_file
+pip install celery
+sudo pip install python-novaclient
 mkdir /home/ubuntu/naca_airfoil/geo
 mkdir /home/ubuntu/naca_airfoil/msh
 mkdir /home/ubuntu/naca_airfoil/xml
 chmod -R 777 /home/ubuntu/naca_airfoil
-cd /home/ubuntu/naca_airfoil
-ip_file=$(head -n 1 ip_number.txt)
-export BROKER_IP=$ip_file
 sudo -H -u ubuntu bash -c "celery worker -A tasks &"
