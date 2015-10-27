7908a8a5eb4bfcd6e65169ff1ea3944bfbe23f86
Andreas Lelli <andreas@lelli.se>
2015-10-27 09:28:36 +0100

    Fixed wrong path for ip file

--- worker_data.sh
+++ worker_data.sh
@@ -7,7 +7,7 @@ mkdir /home/ubuntu/naca_airfoil/geo
 mkdir /home/ubuntu/naca_airfoil/msh
 mkdir /home/ubuntu/naca_airfoil/xml
 chmod -R 777 /home/ubuntu/naca_airfoil
+cd /home/ubuntu/naca_airfoil
 ip_file=$(head -n 1 ip_number.txt)
 export BROKER_IP=$ip_file
-cd /home/ubuntu/naca_airfoil
 sudo -H -u ubuntu bash -c "celery worker -A tasks &"
